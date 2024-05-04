28939974b32c9636fe3f3c288b4e789b8ebafc35
aramusayelyan <musayelyanara6@gmail.com>
2024-05-04 19:44:24 +0400

    Alarm Update

--- Practice/Audible Signal-Based Alarm/main.c
+++ Practice/Audible Signal-Based Alarm/main.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<signal.h>
+#include <ctype.h>
 
 void alarm_hendler(){
 	system("aplay sound.wav");
@@ -10,14 +11,14 @@ void alarm_hendler(){
 int main(){
 int time = 0;
 signal(SIGALRM, alarm_hendler);
+printf("We trust our users and trust that our customers are normal people and for sure when we ask them to enter a number they will not enter some symbol. Thank you for using our program, we are the only one that does not respect the rights of its user. \n\n\nPostscript. Half-Blood Prince: \n");
 do{
 	printf("Enter the period after which you want to receive the alarm։ ");
 	scanf("%i",&time);
 	alarm(time);
 
 	sleep(time);
-	printf("If you want to continue, enter any number, if you want to exit, enter a negative number: ");
-
+	
 }while(time > 0);
 exit(1);
 
